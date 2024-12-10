e7dd947e4dfed9f04e4eecb0a941c02fe7f5617b
bbek4210 <bhandaribibek731@gmail.com>
2024-12-10 17:24:00 +0545

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Hero from "@/components/Hero";
 import { main } from "framer-motion/client";
 import React from "react";
 
 const Home = () => {
-  return <main> </main>;
+  return <main className="h-full w-full">
+    <div className="flex flex-col gap-20">
+
+    </div>
+  </main>;
 };
 
 export default Home;
