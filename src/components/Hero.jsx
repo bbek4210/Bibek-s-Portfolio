8bbc533302394b116a9d3cf373f5f1b91d83f841
bbek4210 <bhandaribibek731@gmail.com>
2024-12-11 22:53:53 +0545

    l

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { profilePic } from "../assets/kevinRushProfile.png";
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mb-36 ">
+    <div className="pb-4 border-b border-neutral-900 lg:mb-36 ">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
@@ -12,18 +12,19 @@ const Hero = () => {
               Bibek Bhandari
             </h1>
             <span
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
-           bg-clip-text text-4xl tracking-tight text-transparent"
+              className="text-4xl tracking-tight text-transparent bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text"
             >
               FrontEnd Developer
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            <p className="max-w-xl py-6 my-2 font-light tracking-tighter">
               {HERO_CONTENT}
             </p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
-            <div className=" "></div>
+            <div className="flex justify-center">
+              <img src={profilePic} alt="profile" />
+            </div>
         </div>
       </div>
     </div>
